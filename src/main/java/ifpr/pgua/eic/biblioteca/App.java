4510d0b3b7390630bdafc0a35f6aae9861629378
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2021-10-20 11:49:48 -0300

    configurando para funcionar com fxml

--- src/main/java/ifpr/pgua/eic/biblioteca/App.java
+++ src/main/java/ifpr/pgua/eic/biblioteca/App.java
@@ -1,9 +1,12 @@
 package ifpr.pgua.eic.biblioteca;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 
 /**
@@ -18,6 +21,24 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    private Parent loadTela(String fxml, Callback controller){
+        Parent root = null;
+        
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource(fxml));
+            loader.setControllerFactory(controller);
+
+            root = loader.load();
+
+        }catch(Exception e){
+            System.out.println("Erro no carregamento do fxml! O arquivo está correto???"+fxml);
+        }
+
+        return root;
+    }
+
 
     public static void main(String[] args) {
         launch();
